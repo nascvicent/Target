08327533960064e9e5fa27fbf4660fb9a56c1635
nascvicent <eminhovicente@hotmail.com>
2024-09-03 15:19:34 -0300

    concerto erro

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,29 +1,40 @@
 public class Fibonacci {
-    public String fibonacci(int n){
-        int[] listaFibo = new int[n + 1];
-        boolean pertence = false;
+    public String fibonacci(int n) {
+        if (n < 0) {
+            return n + " não pertence à Fibonacci.";
+        }
 
+
+        if (n == 0) {
+            return "0 pertence";
+        } else if (n == 1) {
+            return "1 pertence";
+        }
+
+
+        int[] listaFibo = new int[n + 1];
         listaFibo[0] = 0;
         listaFibo[1] = 1;
 
 
-        for(int i = 2; i <= n; i++){
-            listaFibo[i] = listaFibo[i-1] + listaFibo[i-2];
+        for (int i = 2; i <= n; i++) {
+            listaFibo[i] = listaFibo[i - 1] + listaFibo[i - 2];
         }
 
 
-        for(int i = 0; i <= n-1; i++){
+        boolean pertence = false;
+        for (int i = 0; i <= n; i++) {
             System.out.println(listaFibo[i]);
-            if(n == listaFibo[i]){
+            if (n == listaFibo[i]) {
                 pertence = true;
+                break;
             }
         }
 
-
-        if(pertence){
-            return  n +" pertence à Fibonacci.";
+        if (pertence) {
+            return n + " pertence à Fibonacci.";
         } else {
-            return n+" não pertence à Fibonacci.";
+            return n + " não pertence à Fibonacci.";
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-        System.out.println("Primeira questão: " + fibonacci.fibonacci(5));
+        System.out.println("Primeira questão: " + fibonacci.fibonacci(0));
         StringCount stringCount = new StringCount();
         System.out.println("Segunda questão : " + stringCount.verificarLetra("TargetA"));
         Terceira terceira = new Terceira();
